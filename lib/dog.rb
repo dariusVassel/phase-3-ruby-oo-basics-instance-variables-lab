c12482de69185f29ac57b05a5683256e8f67c6bd
+ <+>
2022-02-25 18:02:39 -0500

    commit tree 07ae64142f5e515511ae303afef2aeaf3ecccfc1

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
-end
+    # setter method
+    def name=(value)
+      @this_dogs_name = value
+    end
+  
+    # getter method
+    def name
+      # access the data from the @this_dogs_name instance variable and return it
+      @this_dogs_name
+    end
+  end
+  
+  lassie = Dog.new
+  lassie.name = "Lassie"
+  
+  # call the Dog#this_dogs_name method
+  puts lassie.name
+  # "Odie"
